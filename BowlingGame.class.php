ac25d2b46d89af6cfb24f364c3b87a2febff3e40
BeauvR <beauvrmc140@gmail.com>
2020-05-28 11:43:45 +0200

    add default files

---
+++ BowlingGame.class.php
@@ -0,0 +1,43 @@
+<?php
+
+class BowlingGame
+{
+    private $scoreBoard;
+    private $players;
+    private $round;
+
+    public function __construct()
+    {
+        // TODO
+    }
+
+    public function start()
+    {
+        // TODO
+    }
+
+    private function addPlayer($playerName)
+    {
+        // TODO
+    }
+
+    private function askPlayerNames()
+    {
+        // TODO
+    }
+
+    private function playRound()
+    {
+        // TODO
+    }
+
+    private function playLastRound()
+    {
+        // TODO
+    }
+
+    private function playAllRounds()
+    {
+        // TODO
+    }
+}
---
+++ ScoreBoard.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class ScoreBoard {
+    private $scores;
+
+    public function __construct()
+    {
+        // TODO
+    }
+
+    private function calculatePlayerScore($player)
+    {
+        // TODO
+    }
+
+    public function calculateAllScores()
+    {
+        // TODO
+    }
+
+    public function displayScores()
+    {
+        // TODO
+    }
+
+    public function displayWinner()
+    {
+        // TODO
+    }
+}
