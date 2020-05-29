40146791c2d323b598a3c2239b43a7961ff99466
BeauvR <beauvrmc140@gmail.com>
2020-05-29 12:23:06 +0200

    added bolwling

--- Player.class.php
+++ Player.class.php
@@ -1,16 +1,1 @@
-<?php
-
-class Player {
-    public $name;
-    public $pinsThrown;
-
-    public function __construct($name)
-    {
-        // TODO
-    }
-
-    public function throwPins($throw1, $throw2 = 0)
-    {
-        // TODO
-    }
-}
+<!--Not used-->
